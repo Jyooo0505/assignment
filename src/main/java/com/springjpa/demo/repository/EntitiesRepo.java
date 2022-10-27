f6deb0854170e1509ba917dfc48e34a751cf170d
Jyooo0505 <116811250+Jyooo0505@users.noreply.github.com>
2022-10-27 18:22:05 +0530

    Update EntitiesRepo.java

--- src/main/java/com/springjpa/demo/repository/EntitiesRepo.java
+++ src/main/java/com/springjpa/demo/repository/EntitiesRepo.java
@@ -1,7 +1,13 @@
 package com.springjpa.demo.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.springjpa.demo.modal.Entities;
 public interface EntitiesRepo extends CrudRepository<Entities, Integer>{
 
+	
+	@Query("SELECT count(entity_id) FROM Entities_To_Case e, Cases s WHERE e.id=c.id")
+	List<Entities> findEntities(String caseNumber);
 }
